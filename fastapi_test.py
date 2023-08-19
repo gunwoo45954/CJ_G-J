4866e9b9fc159cc585e9d1d31bde5e9de2f4028d
gunwoo45954 <gunwoo45954@gmail.com>
2023-08-19 11:09:18 +0900

    seq type change

--- fastapi_test.py
+++ fastapi_test.py
@@ -35,7 +35,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
     # requestAddress : 요청 영문주소 (CHAR)
 
 class RequestItem(BaseModel):
-    seq : int = Field(gt = 0, le = 99999999)
+    seq : str = Field(..., max_length=8)
     requestAddress : str = Field(..., max_length=2000)
 
 class RequestJSON(BaseModel):
@@ -78,7 +78,7 @@ class CustomAPIRoute(APIRoute):
                     return JSONResponse(content = "requestiList field is not exist.")
                 
                 for item in request_data.get("requestList", []):
-                    if not isinstance(item.get("seq"), int) or not (1 <= item["seq"] <= 99999999) or not isinstance(item.get("requestAddress"), str) or not (1 <= len(item["requestAddress"]) <= 2000):
+                    if not isinstance(item.get("seq"), str) or not (1 <= len(item["seq"]) <= 8) or not isinstance(item.get("requestAddress"), str) or not (1 <= len(item["requestAddress"]) <= 2000):
                         fail_seq = item.get("seq")
                         raise ValueError()
                     
@@ -104,62 +104,42 @@ def create_response(item : RequestJSON):
     response = ResponseJSON(HEADER=HeaderItem())
     chunk_list = lambda lst,chunk_size : [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
     
+    Large_Size = 100
+    Large = [dict(i) for i in dict(item)['requestList']]
+    
+    Large_Chunk = chunk_list(Large, Large_Size)
+    
+    Total_list = []
+
+    for l in Large_Chunk:
     # 청크로 나눠서 inference 한 후 chatgpt_result 반환
-    chunk_size = 10
-    input_data = [dict(i) for i in dict(item)['requestList']]
-    data_chunk = chunk_list(input_data, chunk_size)
-    
-    
-    lock = Lock()
-    q = Queue()
-    processes = []
-    
-    for i in data_chunk:
-        p = Process(target=inference, args=(api_key, {"requestList":i}, lock, q))
-        processes.append(p)
-        p.start()
-    
-    for p in processes:
-        p.join()
+        chunk_size = 10
+        input_data = [dict(i) for i in l]
+        data_chunk = chunk_list(input_data, chunk_size)
         
-    chatgpt_result = []
-    while not q.empty():
-        chatgpt_result.append(q.get())
-    chatgpt_result = list(chain(*chatgpt_result))
+        lock = Lock()
+        q = Queue()
+        processes = []
+        
+        for i in data_chunk:
+            p = Process(target=inference, args=(api_key, {"requestList":i}, lock, q))
+            processes.append(p)
+            p.start()
+        
+        for p in processes:
+            p.join()
+            
+        chatgpt_result = []
+        while not q.empty():
+            chatgpt_result.append(q.get())
+        chatgpt_result = list(chain(*chatgpt_result))
 
-    body_items = [BodyItem(**{"seq":str(i['seq']),"resultAddress":i['resultAddress']}) for i in chatgpt_result]
-    # input_df = pd.DataFrame(input_data)
-    # result_df = pd.DataFrame(chatgpt_result)
+        body_items = [BodyItem(**{"seq":str(i['seq']),"resultAddress":i['resultAddress']}) for i in chatgpt_result]
+        Total_list.append(body_items)
 
-    # # 재시도할 횟수
-    # retry = 2
-    # for r in range(retry):
-    #     no_answer_df =  input_df[result_df['resultAddress']=='답 없음'] # 답 없음으로 나온 결과들 다시 뽑아내기
-        
-    #     input_data = [{'seq':seq,'requestAddress':address} for seq,address in zip(no_answer_df['seq'],no_answer_df['requestAddress'])]
-    #     data_chunk = chunk_list(input_data, chunk_size)
-        
-    #     q = Queue()
-    #     processes = []
-        
-    #     for i in data_chunk:
-    #         p = Process(target=inference, args=(api_key, {"requestList":i}, lock, q))
-    #         processes.append(p)
-    #         p.start()
-        
-    #     for p in processes:
-    #         p.join()
-        
-    #     chatgpt_result = []
-    #     while not q.empty():
-    #         chatgpt_result.append(q.get())
-    #     chatgpt_result = list(chain(*chatgpt_result))
-        
-    #     for i in chatgpt_result:
-    #         result_df.loc[result_df['seq'] == i['seq'],'resultAddress'] = i['resultAddress']
-
+    Total_items = list(chain(*Total_list))
     
-    response.BODY = body_items
+    response.BODY = Total_items
     return response
 
 app.include_router(custom_router)
